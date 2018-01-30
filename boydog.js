9e7758fe4c30c64c644da5bd9319df0eb1aa2106
Alejandro del Río <adelriosantiago@gmail.com>
2018-01-29 20:26:05 -0600

    Testing dog-rev

--- boydog.js
+++ boydog.js
@@ -23,15 +23,52 @@ module.exports = function(server) {
   var textRev = 1;
   
   
+  //
+  //WebSocket functions and events
+  //
   
-  wss.on('connection', function connection(socket) {
+  
+  //Broadcast to all or to all except a specific client
+  wss.broadcast = function broadcast(data, except) {
+    wss.clients.forEach(function each(client) {
+      if (client !== except && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  };
+  
+  //On a new connection
+  wss.on('connection', function(socket) {
+    console.log("Client connected")
     
-    //Socket events
-    socket.on('message', function incoming(bone) {
-      //
+    socket.on('message', function(bone) {
+      bone = JSON.parse(bone);
+      console.log("message RX", bone)
+      
+      //Revision check
+      if (bone.__rev !== wordRev) {
+        bone = { path: bone.path, val: word, __rev: wordRev };
+        
+        //Middleware
+        
+        bone = JSON.stringify(bone);
+        console.log("rev incorrect, sending", bone);
+        socket.send(bone);
+        
+        return;
+      } else {
+        wordRev = bone.__nextrev;
+        
+        
+        //Middleware
+        
+        bone = JSON.stringify(bone);
+        wss.broadcast(bone);
+        console.log("sent", bone, "except self")
+      }
     })
     
-    socket.on('error', function (err) {
+    socket.on('error', function(err) {
       if (err.code !== 'ECONNRESET') throw err; //Ignore ECONNRESET, throw all else
     })
   });
