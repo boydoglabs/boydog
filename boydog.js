2217fa8c4c84abc664da401f5b9e23dab26ff2c5
Alejandro del Río <adelriosantiago@gmail.com>
2019-02-22 20:57:47 -0600

    Run format

--- boydog.js
+++ boydog.js
@@ -3,19 +3,19 @@
 "use strict";
 
 module.exports = function(server) {
-  var ShareDB = require('sharedb');
-  var WebSocket = require('ws');
-  var WebSocketJSONStream = require('websocket-json-stream');
+  var ShareDB = require("sharedb");
+  var WebSocket = require("ws");
+  var WebSocketJSONStream = require("websocket-json-stream");
   var backend = new ShareDB();
   var connection = backend.connect();
-  
+
   var scope;
-  
-  var doc = connection.get('examples', 'randomABC');
+
+  var doc = connection.get("examples", "randomABC");
   doc.fetch(function(err) {
     if (err) throw err;
     if (doc.type === null) {
-      doc.create({ content: 'abc123' }, () => {
+      doc.create({ content: "abc123" }, () => {
         console.log("created");
       });
       return;
@@ -24,10 +24,10 @@ module.exports = function(server) {
 
   // Connect any incoming WebSocket connection to ShareDB
   var wss = new WebSocket.Server({ server });
-  wss.on('connection', function(ws, req) {
+  wss.on("connection", function(ws, req) {
     var stream = new WebSocketJSONStream(ws);
     backend.listen(stream);
   });
-  
+
   return { doc };
-}
+};
