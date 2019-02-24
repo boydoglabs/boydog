f406c7b555365d265e3b62265a507d2a8d9171aa
Alejandro del Río <adelriosantiago@gmail.com>
2019-02-24 16:46:11 -0600

    Working load 3 elements Rename monitor html

--- boydog.js
+++ boydog.js
@@ -16,12 +16,11 @@ module.exports = function(server) {
   })
   
   //Boydog variables
-  var page;
-  var _mirror = {};
-  var test = 50;
+  var monitor;
+  var documentScope = {};
   var scope;
-
-  var doc = connection.get("examples", "randomABC");
+  
+  /*var doc = connection.get("default", "randomABC");
   doc.fetch(function(err) {
     if (err) throw err;
     if (doc.type === null) {
@@ -30,8 +29,8 @@ module.exports = function(server) {
       });
       return;
     }
-  });
-
+  });*/
+  
   //Connect any incoming WebSocket connection to ShareDB
   var wss = new WebSocket.Server({ server });
   wss.on("connection", function(ws, req) {
@@ -39,16 +38,36 @@ module.exports = function(server) {
     backend.listen(stream);
   });
   
-  var reload = function() {
-    console.log("reloading boy with scope", scope);
+  var restart = function() {
+    console.log("restarting boy with scope", scope);
     
     (async() => {
-      let tt = await page.title();
-      if (!tt) return;
-      await page.focus('#test');
-      await page.keyboard.type('server connected');
+      let hasTitle = await monitor.title();
+      if (!hasTitle) return;
+      
+      Object.keys(scope).forEach((path) => {
+        let value = scope[path];
+        
+        console.log("EACH scope", path, value)
+        documentScope[path] = connection.get("default", path);
+        
+        documentScope[path].fetch(function(err) {
+          if (err) throw err;
+          if (documentScope[path].type === null) {
+            documentScope[path].create({ content: value }, () => {
+              console.log("created", path, "with", value);
+            });
+            return;
+          }
+        });
+      })
+      
+      //TODO: Populate monitor
+      
+      /*//Working writing sample
+      await monitor.focus('#test');
+      await monitor.keyboard.type('server connected');*/
     })();
-    
   }
   
   var attach = function(_scope) {
@@ -58,11 +77,11 @@ module.exports = function(server) {
     
     (async() => {
       const browser = await puppeteer.launch({ headless: false });
-      page = await browser.newPage();
-      await page.goto(`localhost:${ server._connectionKey.split("::::")[1] }`); //Note: This way of getting the server's port may not be very reliable...
-      reload();
+      monitor = await browser.newPage();
+      await monitor.goto(`localhost:${ server._connectionKey.split("::::")[1] }`); //Note: This way of getting the server's port may not be very reliable...
+      restart();
     })();
   };
 
-  return { scope, attach, reload, doc };
+  return { scope, attach, restart };
 };
