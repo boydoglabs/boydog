7db07833951b3d067debf86337d03b44109a5384
Alejandro del Rio <adelriosantiago@gmail.com>
2020-08-28 13:51:57 -0500

    Update to version 2.0.0

---
+++ boydog-client.js
@@ -0,0 +1,90 @@
+const sharedb = require("sharedb/lib/client")
+const ReconnectingWebSocket = require("reconnecting-websocket")
+const Binding = require("sharedb-attribute-binding")
+const $ = require("cash-dom")
+const Swal = require("sweetalert2")
+const _toPath = require("lodash.topath")
+const bdAttributes = ["bd-id", "bd-class", "bd-value", "bd-html", "bd-click"]
+
+// Open WebSocket connection to ShareDB server
+const socket = new ReconnectingWebSocket("ws://" + window.location.host)
+const connection = new sharedb.Connection(socket)
+
+// Status messages // TODO: Move to a small status icon at the bottom-right part of the screen?
+socket.addEventListener("open", function () {
+  Swal.fire({
+    text: "⠀Boydog found",
+    icon: "info",
+    toast: true,
+    timer: 3000,
+    showConfirmButton: false,
+    position: "bottom-end",
+  })
+})
+
+socket.addEventListener("close", function () {
+  Swal.fire({
+    text: "⠀Boydog disconnected",
+    icon: "warning",
+    toast: true,
+    timer: 3000,
+    showConfirmButton: false,
+    position: "bottom-end",
+  })
+})
+
+socket.addEventListener("error", function () {
+  Swal.fire({
+    text: "⠀Boydog connected",
+    icon: "error",
+    toast: true,
+    timer: 3000,
+    showConfirmButton: false,
+    position: "bottom-end",
+  })
+})
+
+// Boydog front-end scope
+let domScope = []
+
+const init = (root = "html") => {
+  bdAttributes.forEach((attr) => {
+    const els = $(`[${attr}]`)
+    if (els.length === 0) return
+    els.each((i, dom) => {
+      // Normalize attr and update document
+      dom.setAttribute(attr, _toPath(dom.getAttribute(attr)).join(">"))
+      const path = dom.getAttribute(attr)
+
+      // Create shareDB document
+      const doc = connection.get("boydog", path)
+      doc.subscribe((err) => {
+        if (err) throw err
+
+        doc.on("op", (op) => {
+          console.log("op on path", path, op)
+        })
+
+        try {
+          new Binding(
+            dom,
+            doc,
+            ["content"],
+            attr.slice(attr.indexOf("-") + 1)
+          ).setup()
+        } catch (err) {
+          if (err.message === "Cannot read property 'content' of undefined") {
+            console.log(
+              `Warning: Unrecognized '${path}' path. Either a typo or path is not a leaf.`
+              // TODO: Show element in red
+            )
+          }
+        }
+      })
+
+      domScope.push({ dom, attr, path, doc })
+    })
+  })
+}
+
+window.boydog = { init }
---
+++ boydog-server.js
@@ -0,0 +1,53 @@
+const path = require("path")
+const ShareDB = require("sharedb")
+const WebSocket = require("ws")
+const WebSocketJSONStream = require("@teamwork/websocket-json-stream")
+const backend = new ShareDB()
+const connection = backend.connect()
+
+// Boydog back-end scope
+let docScope = {}
+
+const init = (scope, server) => {
+  docScope = scope
+
+  //Add "/boydog-client" as an express Express route
+  server._events.request.get("/boydog-client", function (req, res) {
+    return res.sendFile(
+      path.join(__dirname, "static", "dist", "boydog-client.bundle.js")
+    )
+  })
+
+  // Connect any incoming WebSocket connection to ShareDB
+  const wss = new WebSocket.Server({ server })
+  wss.on("connection", (ws) => {
+    backend.listen(new WebSocketJSONStream(ws))
+  })
+
+  const iterate = (root, path = "") => {
+    // TODO: Add support for reading non-leaf elements, these should reply with JSON.stringify(path)
+    Object.entries(root).forEach((e) => {
+      if (Object.prototype.toString.call(e[1]) === "[object Object]") {
+        iterate(e[1], `${path}>${e[0]}`)
+      } else if (Object.prototype.toString.call(e[1]) === "[object Array]") {
+        iterate({ ...e[1] }, `${path}>${e[0]}`)
+      } else {
+        const fullPath = `${path}>${e[0]}`.substr(1)
+        docScope[fullPath] = ((selector) => {
+          // Get document or create it if needed
+          const doc = connection.get("boydog", selector)
+          doc.fetch(function (err) {
+            if (err) throw err
+            if (doc.type === null) return doc.create({ content: String(e[1]) }) // If document doesn't exists then create it
+          })
+          return doc
+        })(fullPath)
+      }
+    })
+  }
+  iterate(docScope)
+
+  console.log("New boydog documents created:", Object.keys(docScope))
+}
+
+module.exports = { init }
