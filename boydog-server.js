2be1ab7be77bf96d4159c21d4f0c46c7c68e5904
Alejandro del Rio <adelriosantiago@gmail.com>
2020-09-13 01:34:35 -0500

    Allow refresh to allow creating extra scope variables

--- boydog-server.js
+++ boydog-server.js
@@ -6,9 +6,90 @@ const backend = new ShareDB()
 const connection = backend.connect()
 
 // Boydog back-end scope
-let docScope = {}
+let refScope = {} // Scope reference
+let docScope = {} // ShareDB docs
+
+const _iterate = (root, path = "") => {
+  // TODO: Add support for reading non-leaf elements, these should reply with JSON.stringify(path)
+  Object.entries(root).forEach((e) => {
+    if (Object.prototype.toString.call(e[1]) === "[object Object]") {
+      _iterate(e[1], `${path}>${e[0]}`)
+    } else if (Object.prototype.toString.call(e[1]) === "[object Array]") {
+      _iterate({ ...e[1] }, `${path}>${e[0]}`)
+    } else {
+      // This is a leaf and needs a shareDB document
+      const fullPath = `${path}>${e[0]}`.substr(1)
+
+      if (docScope[fullPath] !== undefined) return // If document is already created, bailout
+
+      docScope[fullPath] = ((selector) => {
+        // Get document or create it if needed
+        const doc = connection.get("boydog", selector)
+        doc.fetch(function (err) {
+          if (err) throw err
+          if (doc.type === null) {
+            docScope[fullPath]._latestValue = String(e[1])
+            console.log("New document created at:", fullPath)
+            return doc.create({ content: String(e[1]) }) // If document doesn't exists then create it
+          }
+        })
+        return doc
+      })(fullPath)
+
+      // Subscribe after an OP so that we know the latest value
+      docScope[fullPath].subscribe((err) => {
+        if (err) throw err
+        docScope[fullPath].on("op", (op, source) => {
+          docScope[fullPath].fetch((err) => {
+            if (err) throw err
+            docScope[fullPath]._latestValue = docScope[fullPath].data.content // The latest value is saved inside docScope too
+          })
+        })
+      })
+
+      // Define getter so that the latest value can be obtained from the server
+      Object.defineProperty(root, e[0], {
+        // TODO: Define set function, currently the user is not able to set a new scope value from the server
+        set: (v) => {
+          v = String(v)
+          if (v === docScope[fullPath]._latestValue) return
+
+          // TODO: Submit only needed changes. This code works but briefly erases the content from all clients and submits the new content. This sometimes causes the user's caret going to the beginning of the input.
+          docScope[fullPath].submitOp(
+            [{ p: ["content", 0], sd: docScope[fullPath]._latestValue }], // Delete old content on its entirety
+            (err) => {
+              if (err) throw err
+              docScope[fullPath].submitOp(
+                [{ p: ["content", 0], si: v }], // Add new value
+                (err) => {
+                  if (err) throw err
+                }
+              )
+            }
+          )
+        },
+        get: () => {
+          return docScope[fullPath]._latestValue
+        },
+      })
+    }
+  })
+
+  /*// Catch adding new keys to scope // NOTE: This Proxy works only inside boydog-server.js, which is strange. Until we fix this we can use refresh() which is, in any case, faster than a Proxy.
+  scope = new Proxy(scope, {
+    defineProperty(target, key, descriptor) {
+      console.log("def property", key)
+      if (Object.prototype.hasOwnProperty.call(target, key))
+        return Reflect.defineProperty(target, key, descriptor)
+      console.log(`Property ${key} defined.`)
+      return Reflect.defineProperty(target, key, descriptor)
+    },
+  })*/
+}
 
 const init = (scope, server) => {
+  refScope = scope
+
   //Add "/boydog-client" as an express Express route
   server._events.request.get("/boydog-client", function (req, res) {
     return res.sendFile(path.join(__dirname, "boydog-client.bundle.js"))
@@ -20,71 +101,11 @@ const init = (scope, server) => {
     backend.listen(new WebSocketJSONStream(ws))
   })
 
-  const iterate = (root, path = "") => {
-    // TODO: Add support for reading non-leaf elements, these should reply with JSON.stringify(path)
-    Object.entries(root).forEach((e) => {
-      if (Object.prototype.toString.call(e[1]) === "[object Object]") {
-        iterate(e[1], `${path}>${e[0]}`)
-      } else if (Object.prototype.toString.call(e[1]) === "[object Array]") {
-        iterate({ ...e[1] }, `${path}>${e[0]}`)
-      } else {
-        // This is a leaf and needs a shareDB document
-        const fullPath = `${path}>${e[0]}`.substr(1)
-        docScope[fullPath] = ((selector) => {
-          // Get document or create it if needed
-          const doc = connection.get("boydog", selector)
-          doc.fetch(function (err) {
-            if (err) throw err
-            if (doc.type === null) {
-              docScope[fullPath]._latestValue = String(e[1])
-              return doc.create({ content: String(e[1]) }) // If document doesn't exists then create it
-            }
-          })
-          return doc
-        })(fullPath)
-
-        // Subscribe after an OP so that we know the latest value
-        docScope[fullPath].subscribe((err) => {
-          if (err) throw err
-          docScope[fullPath].on("op", (op, source) => {
-            docScope[fullPath].fetch((err) => {
-              if (err) throw err
-              docScope[fullPath]._latestValue = docScope[fullPath].data.content // The latest value is saved inside docScope too
-            })
-          })
-        })
-
-        // Define getter so that the latest value can be obtained from the server
-        Object.defineProperty(root, e[0], {
-          // TODO: Define set function, currently the user is not able to set a new scope value from the server
-          set: (v) => {
-            v = String(v)
-            if (v === docScope[fullPath]._latestValue) return
-
-            // TODO: Submit only needed changes. This code works but briefly erases the content from all clients and submits the new content. This sometimes causes the user's caret going to the beginning of the input.
-            docScope[fullPath].submitOp(
-              [{ p: ["content", 0], sd: docScope[fullPath]._latestValue }], // Delete old content on its entirety
-              (err) => {
-                if (err) throw err
-                docScope[fullPath].submitOp(
-                  [{ p: ["content", 0], si: v }], // Add new value
-                  (err) => {
-                    if (err) throw err
-                  }
-                )
-              }
-            )
-          },
-          get: () => {
-            return docScope[fullPath]._latestValue
-          },
-        })
-      }
-    })
-  }
-  iterate(scope)
-
-  console.log("New boydog documents created:", Object.keys(docScope))
+  _iterate(scope)
 }
 
-module.exports = { init }
+const refresh = () => {
+  _iterate(refScope)
+}
+
+module.exports = { init, refresh }
