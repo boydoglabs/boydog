9d62bd22373dd900d8f9b610c11bba37a96fd01a
Alejandro del Rio <adelriosantiago@gmail.com>
2020-09-18 21:56:12 -0500

    Subscribe and defineProperty only after successfully creating a document

--- boydog-server.js
+++ boydog-server.js
@@ -13,13 +13,12 @@ const _iterate = (root, path = "") => {
   // TODO: Add support for reading non-leaf elements, these should reply with JSON.stringify(path)
   Object.entries(root).forEach((e) => {
     if (Object.prototype.toString.call(e[1]) === "[object Object]") {
-      _iterate(e[1], `${path}>${e[0]}`)
+      return _iterate(e[1], `${path}>${e[0]}`)
     } else if (Object.prototype.toString.call(e[1]) === "[object Array]") {
-      _iterate({ ...e[1] }, `${path}>${e[0]}`)
+      return _iterate({ ...e[1] }, `${path}>${e[0]}`)
     } else {
-      // This is a leaf and needs a shareDB document
+      // This is a leaf
       const fullPath = `${path}>${e[0]}`.substr(1)
-
       if (docScope[fullPath] !== undefined) return // If document is already created, bailout
 
       docScope[fullPath] = ((selector) => {
@@ -28,50 +27,51 @@ const _iterate = (root, path = "") => {
         doc.fetch(function (err) {
           if (err) throw err
           if (doc.type === null) {
+            // Document doesn't exist. Create it.
             docScope[fullPath]._latestValue = String(e[1])
-            console.log("New document created at:", fullPath)
-            return doc.create({ content: String(e[1]) }) // If document doesn't exists then create it
+            doc.create({ content: String(e[1]) })
+            console.log("New document created:", fullPath)
+
+            // Subscribe after an OP so that we know the latest value
+            docScope[fullPath].subscribe((err) => {
+              if (err) throw err
+              docScope[fullPath].on("op", (op, source) => {
+                docScope[fullPath].fetch((err) => {
+                  if (err) throw err
+                  docScope[fullPath]._latestValue = docScope[fullPath].data.content // The latest value is saved inside docScope too
+                })
+              })
+            })
+
+            // Define getter so that the latest value can be obtained from the server
+            Object.defineProperty(root, e[0], {
+              // TODO: Define set function, currently the user is not able to set a new scope value from the server
+              set: (v) => {
+                v = String(v)
+                if (v === docScope[fullPath]._latestValue) return
+
+                // TODO: Submit only needed changes. This code works but briefly erases the content from all clients and submits the new content. This sometimes causes the user's caret going to the beginning of the input.
+                docScope[fullPath].submitOp(
+                  [{ p: ["content", 0], sd: docScope[fullPath]._latestValue }], // Delete old content on its entirety
+                  (err) => {
+                    if (err) throw err
+                    docScope[fullPath].submitOp(
+                      [{ p: ["content", 0], si: v }], // Add new value
+                      (err) => {
+                        if (err) throw err
+                      }
+                    )
+                  }
+                )
+              },
+              get: () => {
+                return docScope[fullPath]._latestValue
+              },
+            })
           }
         })
         return doc
       })(fullPath)
-
-      // Subscribe after an OP so that we know the latest value
-      docScope[fullPath].subscribe((err) => {
-        if (err) throw err
-        docScope[fullPath].on("op", (op, source) => {
-          docScope[fullPath].fetch((err) => {
-            if (err) throw err
-            docScope[fullPath]._latestValue = docScope[fullPath].data.content // The latest value is saved inside docScope too
-          })
-        })
-      })
-
-      // Define getter so that the latest value can be obtained from the server
-      Object.defineProperty(root, e[0], {
-        // TODO: Define set function, currently the user is not able to set a new scope value from the server
-        set: (v) => {
-          v = String(v)
-          if (v === docScope[fullPath]._latestValue) return
-
-          // TODO: Submit only needed changes. This code works but briefly erases the content from all clients and submits the new content. This sometimes causes the user's caret going to the beginning of the input.
-          docScope[fullPath].submitOp(
-            [{ p: ["content", 0], sd: docScope[fullPath]._latestValue }], // Delete old content on its entirety
-            (err) => {
-              if (err) throw err
-              docScope[fullPath].submitOp(
-                [{ p: ["content", 0], si: v }], // Add new value
-                (err) => {
-                  if (err) throw err
-                }
-              )
-            }
-          )
-        },
-        get: () => {
-          return docScope[fullPath]._latestValue
-        },
-      })
     }
   })
 
